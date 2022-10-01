1434a999b3b86a66344ecbd2fdd37ee99048f929
XIAO SAN <106890463+AMBYSBOTZ@users.noreply.github.com>
2022-10-02 05:46:06 +0700

    config.js

--- system/config.js
+++ system/config.js
@@ -1,7 +1,7 @@
 // Owner number
 global.owner = '6285798267620'
 // Owner name
-global.owner_name = 'Customer Service'
+global.owner_name = 'XIAO SAN'
 // Maximum upload file size limit (Default : 50 MB)
 global.max_upload = 50
 // Delay for spamming protection (Default : 3 seconds)
